8c4ce87dc89f6a2fc678a81eb0515b31ac9b2640
Joseph Gleason <joeg1834@gmail.com>
2025-09-14 21:16:01 -0400

    added task4

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -34,3 +34,21 @@ function printTeacher(firstName: string, lastName: string): string {
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+  
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
